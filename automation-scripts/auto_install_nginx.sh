95b3024a010079ce35c07599844f9b1842b8c873
WalsmiT <walsmit2@gmail.com>
2024-12-01 20:06:51 +0300

    Update auto_install_nginx.sh

--- automation-scripts/auto_install_nginx.sh
+++ automation-scripts/auto_install_nginx.sh
@@ -1,31 +1,34 @@
 #!/bin/bash
 
 # Переменная <status_procces> для хранении команды "провреки статуса".
-# Переменная <active_service> для хранения вывода статуса сервиса.
+
 # Функция <install_nginx> проверяет сущетсвует сервис, если да то выводит статус,
 # если нет: устанавливает, проверяет если сервис не запущен, запустить.
-status_procces=$(systemctl is-active nginx)
-active_service=$(systemctl status nginx | grep "Active")
-
 install_nginx() {
+    status_procces=$(systemctl is-active nginx)
 
-	if [ "$status_procces" == "active" ]; then
-		echo "Сервис NGINX устрановлен"
+    if [ "$status_procces" == "active" ]; then
+        echo "Сервис NGINX уже установлен и работает."
 
-	else 
-        	echo "Запускаю установку NGINX"
-        	sudo apt update
-        	sudo apt install -y nginx
+    else
+        echo "Запускаю установку NGINX..."
+        sudo apt update
+        sudo apt install -y nginx
 
-		if [ "$status_procces" == "inactive" ]; then
-                	sudo systemctl start nginx
-		fi
-	fi
-	
-	echo "NGINX готов к работе!"
-	sudo systemctl status nginx
+
+        status_procces=$(systemctl is-active nginx)
+
+        # Если сервис не запущен, запускаем его
+        if [ "$status_procces" == "inactive" ] || [ "$status_procces" == "failed" ]; then
+            echo "Запускаю сервис NGINX..."
+            sudo systemctl start nginx
+        fi
+    fi
+    
+    # Выводим информацию о статусе NGINX после установки или запуска
+    echo "NGINX готов к работе!"
+    sudo systemctl status nginx
 }
-
 
 install_nginx
 
